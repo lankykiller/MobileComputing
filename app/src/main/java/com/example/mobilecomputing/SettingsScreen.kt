d8b3ed2e42daa7772435ca3b4c8b8835e2c09a64
lankykiller <dani.mwai@gmail.com>
2025-01-28 17:22:32 +0200

    hw2

--- app/src/main/java/com/example/mobilecomputing/SettingsScreen.kt
+++ app/src/main/java/com/example/mobilecomputing/SettingsScreen.kt
@@ -1,17 +1,18 @@
 package com.example.mobilecomputing
 
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,34 +21,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(navController: NavHostController) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Image(
-                painter = painterResource(R.drawable.backbutton),
-                contentDescription = "Back button",
-                modifier = Modifier
-                    .size(40.dp)
-                    .clickable { navController.popBackStack() }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Settings") },
+                navigationIcon = {
+                    IconButton(onClick = {navController.popBackStack()}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back button"
+                        )
+                    }
+                },
             )
-        }
+        },
+        content = { innerPadding ->
 
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -66,5 +69,6 @@ fun SettingsScreen(navController: NavHostController) {
                     .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
         }
-    }
+        }
+    )
 }
